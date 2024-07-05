dc6713a947540a0f5618dc2290907c4be020f4e8
bilol <bilolnorov7@gmail.com>
2024-07-05 17:38:23 +0500

    bug fix

--- config/urls.py
+++ config/urls.py
@@ -1,7 +1,27 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
+from django.views.static import serve
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from rest_framework.permissions import AllowAny
 
+from django.conf import settings
+from django.conf.urls.static import static
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="UzMovie API",
+
+        default_version='v1', ),
+    public=True,
+    permission_classes=[AllowAny, ],
+)
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('blog.urls')),
+
+    path('docs/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+
+    re_path(r'media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+    re_path(r'static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT})
 ]
