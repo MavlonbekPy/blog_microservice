16dae5505d1649b4e2b04275439a304994d232d5
Mavlonbek <er.mavlon19@gmail.com>
2024-07-05 18:52:17 +0500

    like models.py and requirements.txt added

--- blog/models.py
+++ blog/models.py
@@ -36,6 +36,18 @@ class Post(models.Model):
         return self.title
 
 
+class Like(models.Model):
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE
+    )
+    post = models.ForeignKey(
+        Post, on_delete=models.CASCADE
+    )
+
+    def __str__(self):
+        return self.user
+
+
 class Comment(models.Model):
     author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
     post = models.ForeignKey(Post, on_delete=models.SET_NULL, null=True)
