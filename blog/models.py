4811df1204f498cd3356cc054ac73cd4b1a75aa4
Mavlonbek <er.mavlon19@gmail.com>
2024-07-05 19:34:31 +0500

    Tag deleted

--- blog/models.py
+++ blog/models.py
@@ -9,13 +9,6 @@ class Category(models.Model):
         return self.name
 
 
-class Tag(models.Model):
-    name = models.CharField(max_length=120)
-
-    def __str__(self):
-        return self.name
-
-
 class Post(models.Model):
     author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
     title = models.CharField(max_length=120)
@@ -23,7 +16,6 @@ class Post(models.Model):
     image = models.ImageField(upload_to='posts/')
 
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
-    tags = models.ManyToManyField(Tag)
 
     like_count = models.IntegerField(default=0)
     comment_count = models.IntegerField(default=0)
