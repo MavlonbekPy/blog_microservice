1958fc1b65075097c4f8dec80fcffd1a9b84b28c
bilol <bilolnorov7@gmail.com>
2024-07-10 19:39:46 +0500

    bug

--- blog/urls.py
+++ blog/urls.py
@@ -3,19 +3,10 @@ from rest_framework import views
 from .views import PostViewSet
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
-posts_list = PostViewSet.as_view({
-    'get': 'list',
-    'post': 'create'
-})
-posts_detail = PostViewSet.as_view({
-    'get': 'retrieve',
-    'put': 'update',
-})
-
 urlpatterns = [
     path('posts/', PostViewSet.as_view({"get": "get_posts"})),
     path('post/delete/', PostViewSet.as_view({"delete": "post_delete"})),
-    path('post/create/', PostViewSet.as_view({"put": "create_post"})),
+    path('post/create/', PostViewSet.as_view({"post": "create_post"})),
     path('post/<int:pk>/', PostViewSet.as_view({"get": "retrieve"})),
     path('like-unlike/', PostViewSet.as_view({"post": "like_unlike_post"})),
 ]
